6276d3689ea7bf6ba2cf7caa6a7ddef8d80212ed
Manij Shrestha <manijshrestha@gmail.com>
2017-06-02 16:03:18 -0500

    RxJava2 and Room

--- app/src/main/java/com/manijshrestha/todolist/data/TaskDao.kt
+++ app/src/main/java/com/manijshrestha/todolist/data/TaskDao.kt
@@ -2,11 +2,12 @@ package com.manijshrestha.todolist.data
 
 import android.arch.persistence.room.*
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
+import io.reactivex.Flowable
 
 @Dao interface TaskDao {
 
     @Query("select * from task")
-    fun getAllTasks(): List<Task>
+    fun getAllTasks(): Flowable<List<Task>>
 
     @Query("select * from task where id = :p0")
     fun findTaskById(id: Long): Task
--- app/src/main/java/com/manijshrestha/todolist/ui/ToDoPresenter.kt
+++ app/src/main/java/com/manijshrestha/todolist/ui/ToDoPresenter.kt
@@ -2,10 +2,15 @@ package com.manijshrestha.todolist.ui
 
 import com.manijshrestha.todolist.data.Task
 import com.manijshrestha.todolist.data.TaskDao
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class ToDoPresenter @Inject constructor(val taskDao: TaskDao) {
 
+    val compositeDisposable = CompositeDisposable()
     var tasks = ArrayList<Task>()
 
     var presentation: ToDoPresentation? = null
@@ -16,22 +21,31 @@ class ToDoPresenter @Inject constructor(val taskDao: TaskDao) {
     }
 
     fun onDestroy() {
+        compositeDisposable.dispose()
         presentation = null
     }
 
     fun loadTasks() {
-        tasks.clear()
-        tasks.addAll(taskDao.getAllTasks())
+        compositeDisposable.add(taskDao.getAllTasks()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    tasks.clear()
+                    tasks.addAll(it)
+                    (tasks.size - 1).takeIf { it >= 0 }?.let {
+                        presentation?.taskAddedAt(it)
+                        presentation?.scrollTo(it)
+                    }
+                }))
+
         presentation?.showTasks(tasks)
     }
 
     fun addNewTask(taskDescription: String) {
         val newTask = Task(description = taskDescription)
-        tasks.add(newTask)
-        taskDao.insertTask(newTask)
-        (tasks.size - 1).let {
-            presentation?.taskAddedAt(it)
-            presentation?.scrollTo(it)
-        }
+        compositeDisposable.add(Observable.fromCallable { taskDao.insertTask(newTask) }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe())
     }
 }
